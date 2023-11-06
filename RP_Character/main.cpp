2ef8936f566da2e0bff16e53c749f043b21e00fd
epth <epth@C21Y22M8>
2023-11-06 18:03:41 +0500

    Добавьте файлы проекта.

---
+++ RP_Character/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+#include<cmath>
+
+class Character {
+public:
+	Character() {
+
+	}
+
+
+
+
+
+private:
+	int hp;
+	std::string gender, name, race;
+};
+
+
+
+
+
+
+
+
+
+int main() {
+
+
+
+
+
+	return 0;
+}
